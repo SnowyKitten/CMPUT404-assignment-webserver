fe65c5fa83e17edb0511d82cfae042514f71bcdb
SnowyKitten <rtwong@ualberta.ca>
2016-01-12 12:37:32 -0700

    Project completion

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
 import SocketServer
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+
+# Copyright 2016 Abram Hindle, Eddie Antonio Santos, Randy Wong, Marcin Pietrasik
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,8 +32,39 @@ class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        readRequest = self.data.split("\r")
+        HTTPRequest = readRequest[0]
+        requestedResource = self.HTTPParser(HTTPRequest)
+
+        # if we end our query with /, we want the index.html in that folder
+        if (requestedResource[-1] == "/"):
+            requestedResource = requestedResource + "index.html"
+
+        try:
+            # handle client being rude, send them a 404
+            if "/.." in requestedResource:
+               raise
+
+            resource = open("www" + requestedResource, "r")
+            # send success header
+            self.request.sendall("HTTP/1.1 200 OK\r\n")
+            # finds the MIME extension for the resource
+            fileType = requestedResource.split(".")[-1]
+            # sends header line with extension type
+            self.request.sendall("Content-Type: text/" + fileType + ";\r\n\r\n")
+            # send resource
+            self.request.sendall(resource.read())
+
+        except:
+            # can't open a resource, not found, or directed here when client tries to use /..
+            self.request.sendall("HTTP/1.1 404 Not Found\r\n\r\n 404 Not Found")
+
+
+    def HTTPParser(self, HTTPString):
+        pageRequest = HTTPString.split(" ")
+        if pageRequest[0] != "GET":
+            return -1
+        return pageRequest[1]
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
